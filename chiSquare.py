a51e31235e238c819f1bf70ad10d7ac9cb432ed4
WraasTheOne <nicholas.mazur.hansen@gmail.com>
2024-02-22 12:21:33 +0100

    små ændringer

--- chiSquare.py
+++ chiSquare.py
@@ -4,6 +4,7 @@ def chi_squared(observed, expected):
    
     observed = np.array(observed)
     expected = np.array(expected)
+    print((observed - expected)**2 / expected)
     
     # Calculate the chi-squared statistic
     chi_squared_stat = np.sum((observed - expected)**2 / expected)
@@ -11,7 +12,7 @@ def chi_squared(observed, expected):
     return chi_squared_stat
 
 # Example usage:
-observed_freq = [13, 6, 13, 6, 9,9,11,15,9,9]
+observed_freq = [4, 13, 11, 15, 7, 10 , 8 ,10 , 8 , 14]
 expected_freq = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 
 chi_squared_value = chi_squared(observed_freq, expected_freq)
